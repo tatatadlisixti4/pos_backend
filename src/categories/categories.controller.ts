3a6cf656ea57724dbd26854c7f46e721acf232a5
Thadli Guerra <tg.ibarra64@gmail.com>
2025-03-03 00:19:23 -0300

    Registrando el repositorio en el service

--- src/categories/categories.controller.ts
+++ src/categories/categories.controller.ts
@@ -12,13 +12,14 @@ import { CategoriesService } from './categories.service';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
 
-@Controller('categories')
+@Controller('categories')  
 export class CategoriesController {
-  constructor(private readonly categoriesService: CategoriesService) {}
+  constructor(
+    private readonly categoriesService: CategoriesService
+  ) {}
 
   @Post()
   create(@Body() createCategoryDto: CreateCategoryDto) {
-    console.log(createCategoryDto);
     return this.categoriesService.create(createCategoryDto);
   }
 
--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import {Repository} from 'typeorm'
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
+import { Category } from './entities/category.entity';
 
 @Injectable()
 export class CategoriesService {
+  constructor(
+    @InjectRepository(Category) 
+    private readonly categoryRepository : Repository<Category>
+  ) {}
   create(createCategoryDto: CreateCategoryDto) {
     return createCategoryDto;
   }
