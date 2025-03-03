52dfb9f9935253912713d585d71c706bf8dfa440
Thadli Guerra <tg.ibarra64@gmail.com>
2025-03-02 23:26:49 -0300

    Registrando entidades

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,5 +1,6 @@
 import { ConfigService } from "@nestjs/config"
 import type { TypeOrmModuleOptions } from "@nestjs/typeorm"
+import { join } from "path"
 export const typeOrmConfig = (configService: ConfigService) : TypeOrmModuleOptions => ({
     type: 'postgres',
     host: configService.get('DATABASE_HOST'),
@@ -7,5 +8,7 @@ export const typeOrmConfig = (configService: ConfigService) : TypeOrmModuleOptio
     username: configService.get('DATABASE_USER'),
     database: configService.get('DATABASE_NAME'),
     port: configService.get('DATABASE_PORT'),
-    logging: true
+    logging: true,
+    entities: [join(__dirname, '../**/*.entity.{js,ts}')],
+    synchronize: true
 })
